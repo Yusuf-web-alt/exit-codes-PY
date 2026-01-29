7ae98a0818f5312b1c51a829a3bc23244228809d
Yusuf <YusufMuizzudin@outlook.com>
2026-01-29 00:51:13 +0000

    Update exitcodes.py

--- exitcodes.py
+++ exitcodes.py
@@ -1,6 +1,5 @@
 import sys
-import random
 exits = ["3489660927", "1073741819", "1073740791", "1", "5", "0"]
 
-#for exit in exits:
-sys.exit(random.choice(exits))
+for exit in exits:
+    print(exit)
